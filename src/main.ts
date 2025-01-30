9028aa618291019776388c031e39ba52159816c4
victor martinez <vitico841@gmail.com>
2025-01-30 11:26:11 -0400

    Agrega soporte para microservicios de NATS y actualiza la configuración de arranque

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const context = await NestFactory.createApplicationContext(AppModule);
+  const configService = context.get(ConfigService);
+  const NATS_URL = configService.get<string>('NATS_URL');
+  await context.close();
+  const logger = new Logger('Main')
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: [NATS_URL],
+      },
+    },
+  );
+
+  await app.listen()
+  logger.log('Shipping-Address-Microservice is running')
 }
 bootstrap();
